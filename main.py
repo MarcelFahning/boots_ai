050a8317b7ab615ebcd36204d738f4ee081b3372
Marcel Fahning <github@fahning.me>
2025-08-28 11:05:40 +0200

    removed hardcodec prompt and added cli argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,7 +7,11 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 def main():
-    generated_content = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if len(sys.argv) < 2:
+        print("Usage: uv run main.py <prompt>")
+        sys.exit(1)
+    prompt = sys.argv[1]
+    generated_content = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
     print(generated_content.text)
     print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
