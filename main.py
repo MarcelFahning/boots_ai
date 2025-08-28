756cc97a413106bad6edf0d05639ae8fd2094fcb
Marcel Fahning <github@fahning.me>
2025-08-28 11:14:25 +0200

    added --verbose argument

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai
+from google import genai, types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,11 +10,18 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: uv run main.py <prompt>")
         sys.exit(1)
-    prompt = sys.argv[1]
-    generated_content = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    user_prompt = sys.argv[1]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    is_verbose = False
+    if sys.argv[2] == "--verbose":
+        is_verbose = True
+    generated_content = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     print(generated_content.text)
-    print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
+    if is_verbose:
+        print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
