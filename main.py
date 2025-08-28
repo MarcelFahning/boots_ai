51f51f6267d64d7cf03f334fc83e9061c2ff51c6
Marcel Fahning <github@fahning.me>
2025-08-28 10:51:25 +0200

    fixed generate_content call with named parameters

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 def main():
-    generated_content = client.models.generate_content("gemini-2.0-flash-001", "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    generated_content = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
     print(generated_content.text)
     print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
