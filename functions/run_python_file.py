259d4275ba56f8390784140122b499cd8984bc8e
Marcel Fahning <github@fahning.me>
2025-08-29 08:14:05 +0200

    initial commit

---
+++ functions/run_python_file.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    # checks if directory ends up inside the working path
+    abs_work = os.path.abspath(working_directory)
+    abs_target = os.path.abspath(os.path.join(working_directory, file_path))
+    rel_path = os.path.relpath(abs_target, abs_work)
+    if rel_path == ("..") or rel_path.startswith(f"..{os.sep}"):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_target):
+        return f'Error: File "{file_path}" not found.'
+    if not abs_target.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        completed_process = subprocess.run(["python3", abs_target] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=working_directory, timeout=30)
+
+        output_string = f"STDOUT: {completed_process.stdout}\nSTDERR: {completed_process.stderr}"
+        if completed_process.returncode != 0:
+            output_string += f"\nProcess exited with code {completed_process.returncode}"
+        if output_string == "":
+            output_string = "No output produced"
+        return output_string
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    
